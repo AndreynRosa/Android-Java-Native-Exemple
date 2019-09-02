a97675c16ea1582a6f1972239bd0d7de3feb5d1d
Andrey <andreynrosa@gmail.com>
2019-09-02 19:46:22 -0300

    ainda não finalizado o resultado

--- app/src/main/java/com/example/boletimapp/view/ResultadoActivity.java
+++ app/src/main/java/com/example/boletimapp/view/ResultadoActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.boletimapp.R;
+import com.example.boletimapp.view.controller.ResultadoController;
 
 public class ResultadoActivity extends AppCompatActivity {
 
+    private ResultadoController controller;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+        controller = new ResultadoController(this);
     }
+
+
 }
--- app/src/main/java/com/example/boletimapp/view/controller/ResultadoController.java
+++ app/src/main/java/com/example/boletimapp/view/controller/ResultadoController.java
@@ -1,4 +1,40 @@
 package com.example.boletimapp.view.controller;
 
+import android.app.Activity;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+
+import com.example.boletimapp.R;
+import com.example.boletimapp.view.model.Entity.Disciplina;
+
+import java.util.List;
+
 public class ResultadoController {
+    private Activity activity;
+    private TextView textoDisiplina01;
+    private TextView textoDisiplina02;
+
+
+    public ResultadoController(Activity activity) {
+        this.activity = activity;
+        initComponents();
+        getDisciplina();
+    }
+
+    private void initComponents() {
+        textoDisiplina02 = activity.findViewById(R.id.textoDisiplina01);
+        textoDisiplina02 = activity.findViewById(R.id.textoDisiplina02);
+    }
+
+    private void getDisciplina() {
+        List<Disciplina> disciplinas = (List<Disciplina>)
+                activity.getIntent().getSerializableExtra("disciplinas");
+        for (Disciplina disciplina : disciplinas) {
+            System.out.println(disciplina.toString());
+        }
+
+    }
 }
+
+
